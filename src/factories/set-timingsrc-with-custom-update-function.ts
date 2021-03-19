b4fbed966f5b5564ecbaeb9c2d4b76416ab050c0
Marcus Schreiter <marcus.schreiter@meso.design>
2021-03-19 13:32:32 +0100

    dont pause video when it's ended

--- src/factories/set-timingsrc-with-custom-update-function.ts
+++ src/factories/set-timingsrc-with-custom-update-function.ts
@@ -13,7 +13,6 @@ export const createSetTimingsrcWithCustomUpdateFunction: TSetTimingsrcWithCustom
 
             if (position >= sanitizedDuration) {
                 mediaElement.currentTime = sanitizedDuration;
-                pause(mediaElement);
                 return;
             }
 
